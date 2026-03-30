b0ffd49b3ed052958ef56a7d1cdacaf060a2e1e3
Sarin <sarinms777@gmail.com>
2026-03-30 20:32:54 +0530

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+import ToDoList from './todo.jsx'
+function App() {
+
+  return (
+    <ToDoList />
+  )
+}
+
+export default App
---
+++ src/todo.jsx
@@ -0,0 +1,32 @@
+import React, {useState} from "react";
+
+function ToDoList(){
+
+    function handleInputChange(event){
+
+    }
+
+    function addTask(){
+
+    }
+
+    function deleteTask(){
+
+    }
+
+    function movetaskUp(){
+
+    }
+
+    function movetaskDown(){
+        
+    }
+    
+
+    return(
+
+        <h1 className="text-center text-6xl font-bold">TODO LIST</h1>
+    )
+}
+
+export default ToDoList
